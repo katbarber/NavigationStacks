132638d9ab314beff31aa93c65ecce1a126df7ce
Kat <kbarber876@gmail.com>
2024-07-29 09:56:14 -0400

    Initial Commit

---
+++ NavigationStacks/NavigationStacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStacksApp.swift
+//  NavigationStacks
+//
+//  Created by Kat Barber on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
